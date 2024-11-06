857b750fbe7b3bc28f2df7c34fea680f9edcf668
MacBookPro <macbookpro@MacBookPros-MacBook-Pro.local>
2024-11-06 17:38:23 +0530

    6 nov

--- context-api-project/README.md
+++ context-api-project/README.md
@@ -1,2 +1,51 @@
 
-Context API  
+Context API 
+
+The React Context API is a built-in feature that allows developers to share data between components in a React application without manually passing props down through every level of the component tree. This can be useful for managing complex state and keeping track of changes.
+
+
+
+Make a context folder in Src.
+then make UserContext.js file in context folder.
+In UserContext.js
+
+import React from "react";
+
+const UserContext= React.createContext();
+
+export default UserContext;
+
+//This UserContext is a provider.
+
+Every Context object comes with a Provider React component that allows consuming components to subscribe to context changes. The Provider component accepts a value prop to be passed to consuming components that are descendants of this Provider. One Provider can be connected to many consumers.
+***********************************************************************************
+
+Now we will make provider so we will create UserContextProvider.jsx file in context folder.
+
+import React from "react";
+import UserContext from "./UserContext";
+
+const UserContextProvider=()=>{
+
+}
+
+export default UserContextProvider;
+
+
+
+import React, { useState } from "react";
+import UserContext from "./UserContext";
+
+const UserContextProvider=({children})=>{
+       const [user,setUser]=useState(null);
+    return(
+        <UserContext.Provider value={{user,setUser}}>
+            {children}
+        </UserContext.Provider>
+    )
+}
+
+export default UserContextProvider;
+
+
+***********************************************************************************
--- formik_form_task/src/App.js
+++ formik_form_task/src/App.js
@@ -12,25 +12,54 @@ const formvalidation = Yup.object().shape({
     .max(12, "too long surname")
     .required("Required"),
   email: Yup.string().email("Invalid Email").required("Required"),
-  image: Yup.mixed().required("Image Required"),
+  // image: Yup.mixed().required("Image Required"),
 });
 
 function App() {
-  const [previewImage, setPreviewImage] = useState(null);
+  // const [previewImage, setPreviewImage] = useState(null);
   const [data, setData] = useState([]);
+  const [editingIndex, setEditingIndex] = useState(null);
+
+  const handleDeleteQuestion = (index) => {
+    setData((prev) => prev.filter((item, i) => i !== index));
+  };
+
+  const handleEditQuestion = (index) => {
+    setEditingIndex(index);
+    console.log(index);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-orange-200">
       <div className=" w-full max-w-md rounded-2xl bg-gray-300 p-6 mb-8">
         <Formik
-          initialValues={{ name: "", surname: "", email: "", image: null }}
+          initialValues={{
+            name: editingIndex !== null ? data[editingIndex].name : "",
+            surname: editingIndex !== null ? data[editingIndex].surname : "",
+            email: editingIndex !== null ? data[editingIndex].email : "",
+          }}
+
           validationSchema={formvalidation}
-          onSubmit={(value) => {
-            setPreviewImage(URL.createObjectURL(value.image));
-            setData([...data,{name: value.name,surname: value.surname,email: value.email}]);
-            console.log(data);
+          enableReinitialize={true}
+          onSubmit={(value, { resetForm }) => {
+            // setPreviewImage(URL.createObjectURL(value.image));
+
+            if (editingIndex !== null) {
+              let updatedData = data;
+              updatedData[editingIndex] = value;
+              setData(updatedData);
+              setEditingIndex(null);
+            } else {
+              setData([...data, value]);
+            }
+            resetForm();
           }}
         >
-          {({ setFieldValue }) => (
+          {(
+            {
+              /*setFieldValue,*/
+            }
+          ) => (
             <Form>
               <div>
                 <div className="flex">
@@ -84,7 +113,7 @@ function App() {
                 />
               </div>
 
-              <div>
+              {/* <div>
                 <div className="flex">
                   <label htmlFor="">Image:</label>
                   <input
@@ -104,23 +133,23 @@ function App() {
                   component="div"
                   style={{ color: "red" }}
                 />
-              </div>
+              </div> */}
 
               <button
                 type="submit"
                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
               >
-                Submit
+                {editingIndex === null ? "Submit" : "Update"}
               </button>
             </Form>
           )}
         </Formik>
 
-        {previewImage && (
+        {/* {previewImage && (
           <div>
             <img src={previewImage} alt="" className="w-48 h-48" />
           </div>
-        )}
+        )} */}
       </div>
 
       <div className="relative overflow-x-auto rounded-2xl">
@@ -136,6 +165,9 @@ function App() {
               <th scope="col" className="px-6 py-3">
                 Email
               </th>
+              <th scope="col" className="px-6 py-3">
+                Actions
+              </th>
             </tr>
           </thead>
           {data &&
@@ -143,25 +175,41 @@ function App() {
             data.map((item, index) => (
               <tbody key={index}>
                 <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                  <th
+                  <td
                     scope="row"
                     className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                   >
                     {item.name}
-                  </th>
+                  </td>
 
-                  <th
+                  <td
                     scope="row"
                     className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                   >
                     {item.surname}
-                  </th>
-                  <th
+                  </td>
+                  <td
                     scope="row"
                     className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                   >
                     {item.email}
-                  </th>
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => {
+                        handleEditQuestion(index);
+                      }}
+                      className="outline-none bg-green-600 text-white px-3 py-0.5 hover:bg-green-700"
+                    >
+                      Edit
+                    </button>
+                    <button
+                      onClick={() => handleDeleteQuestion(index)}
+                      className="outline-none bg-red-600 text-white px-3 py-0.5 hover:bg-red-700"
+                    >
+                      Delete
+                    </button>
+                  </td>
                 </tr>
               </tbody>
             ))}
